2fa8b95dca8284e2567dbe5873d8a42c6a7a2a7c
kajalshanto <hasibulhasan.kajal@gmail.com>
2021-01-14 00:29:12 +0600

    routes updates

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [GuzzleClientController::class, 'getAllData'])->name('getpost.all');
-Route::get('/posts/{id}', [GuzzleClientController::class, 'getSingleData'])->name('getpost.single');
+Route::get('/posts', [GuzzleClientController::class, 'getAllData'])->name('post.all');
+Route::post('/posts', [GuzzleClientController::class, 'storeData'])->name('post.store');
+Route::get('/posts/{id}', [GuzzleClientController::class, 'SingleData'])->name('post.read');
+Route::get('/posts/{id}', [GuzzleClientController::class, 'editData'])->name('post.edit');
+Route::put('/posts/{id}', [GuzzleClientController::class, 'updateData'])->name('post.update');
 
 ##Delte here showing data thats why get but in application we have to use delete
-Route::get('/delete-posts/{id}', [GuzzleClientController::class, 'deleteData'])->name('deletepost.single');
+Route::get('/delete-posts/{id}', [GuzzleClientController::class, 'deleteData'])->name('post.delete');
